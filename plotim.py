cfeeaa84b6979f39044283f78300fcab0397b6ee
tbartos14 <tbartos14@gmail.com>
2017-01-17 11:44:13 -0500

    Add files via upload

--- plotim.py
+++ plotim.py
@@ -1,6 +1,8 @@
 # # P L O T I M # #
 
-#version 0.5.3, 1/13/2017
+#version 0.5.4, 1/17/2017
+
+#readme needs update for residual plots
 
 
 import tkinter as tk
@@ -12,9 +14,9 @@ def vertical_text(text):
     return newtext
 
 class linear_plot(object):
-    def __init__(self, windowx = 600, windowy = 400, bordernorth = 50, bordersouth = 50, bordereast = 30, borderwest = 50, title = "Linear Plot",\
-                 sortpoints = True, draw_lines = True, draw_points = False, ytitle = "y", xtitle = "x", line_color = "#0000bb", image = None,\
-                 line_of_best_fit = False):
+    def __init__(self, windowx=600, windowy=400, bordernorth=50, bordersouth=50, bordereast=30, borderwest=50, title="Linear Plot",\
+                 sortpoints=True, draw_lines=True, draw_points=False, ytitle="y", xtitle="x", line_color="#0000bb", image=None,\
+                 line_of_best_fit=False):
         self.windowx = windowx
         self.windowy = windowy
         self.bordernorth = bordernorth
@@ -33,6 +35,8 @@ class linear_plot(object):
         self.linecolor = line_color
         self.image = image
         self.line_of_best_fit = line_of_best_fit
+        self.residualsx = []
+        self.residualsy = []
     def set_data(self, x, y):
         self.xpoints = []
         self.ypoints = []
@@ -63,10 +67,10 @@ class linear_plot(object):
             self.master = window
         else:
             self.master = tk.Tk()
-        self.canvas = tk.Canvas(self.master, width = self.windowx, height = self.windowy)
-        self.canvas.create_text(self.borderwest + self.graphx/2,self.bordernorth/2, text = self.title)
-        self.canvas.create_text(self.borderwest/3, self.bordernorth + self.graphy/2, text = self.yaxistitle)
-        self.canvas.create_text(self.borderwest + self.graphx/2, self.windowy - self.bordersouth/3, text = self.xaxistitle)
+        self.canvas = tk.Canvas(self.master, width=self.windowx, height=self.windowy)
+        self.canvas.create_text(self.borderwest + self.graphx/2,self.bordernorth/2, text=self.title)
+        self.canvas.create_text(self.borderwest/3, self.bordernorth + self.graphy/2, text=self.yaxistitle)
+        self.canvas.create_text(self.borderwest + self.graphx/2, self.windowy - self.bordersouth/3, text=self.xaxistitle)
         self.master.title(self.title)
         self.canvas.create_rectangle(self.bordernorth, self.borderwest, (self.windowx - self.bordereast), (self.windowy - self.bordersouth), fill="white", outline="white")
         
@@ -127,16 +131,16 @@ class linear_plot(object):
         # now we determine y
         for increment in range(0, self.yincrement + 1):
             self.canvas.create_line(self.borderwest + 1, (self.windowy - self.bordersouth)-(increment/self.yincrement)*(self.windowy-self.bordernorth - self.bordersouth),\
-                                    self.borderwest + self.graphx, (self.windowy - self.bordersouth)-(increment/self.yincrement)*(self.windowy - self.bordernorth - self.bordersouth), fill = "#bbbbbb", dash = (2,2))
+                                    self.borderwest + self.graphx, (self.windowy - self.bordersouth)-(increment/self.yincrement)*(self.windowy - self.bordernorth - self.bordersouth), fill="#bbbbbb", dash=(2,2))
             self.canvas.create_text(self.borderwest - 12, (self.windowy - self.bordersouth)-(increment/self.yincrement)*(self.windowy - self.bordernorth - self.bordersouth),\
-                                    text = "{0:4.4}".format((int((min(self.ypoints))*(10**(self.yrangefactor))))/(10**(self.yrangefactor))+((increment)*(10**(-1*self.yrangefactor)))))
+                                    text="{0:4.4}".format((int((min(self.ypoints))*(10**(self.yrangefactor))))/(10**(self.yrangefactor))+((increment)*(10**(-1*self.yrangefactor)))))
             
         # determining x
         for increment in range(0, self.xincrement + 1):
             self.canvas.create_line(self.bordersouth + (increment/self.xincrement)*(self.windowx - self.bordereast - self.borderwest), (self.windowy - self.bordersouth) - 1,\
-                                    self.bordersouth + (increment/self.xincrement)*(self.windowx - self.bordereast - self.borderwest), (self.windowy - self.bordersouth - self.graphy), fill = "#bbbbbb", dash = (2,2))
+                                    self.bordersouth + (increment/self.xincrement)*(self.windowx - self.bordereast - self.borderwest), (self.windowy - self.bordersouth - self.graphy), fill="#bbbbbb", dash=(2,2))
             self.canvas.create_text(self.borderwest  + (increment/self.xincrement)*(self.windowx - self.bordereast - self.borderwest), (self.windowy - self.bordersouth) + 12,\
-                                    text = ((int((min(self.xpoints))*(10**(self.xrangefactor))))/(10**(self.xrangefactor))+((increment)*(10**(-1*self.xrangefactor)))))
+                                    text=((int((min(self.xpoints))*(10**(self.xrangefactor))))/(10**(self.xrangefactor))+((increment)*(10**(-1*self.xrangefactor)))))
             
         # line of best fit
         n = len(self.xpoints)
@@ -158,7 +162,7 @@ class linear_plot(object):
             self.canvas.create_line((bestfitstart[0] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest,\
                                     (self.windowy - self.bordersouth) - (bestfitstart[1] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin),\
                                     (bestfitend[0] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest,\
-                                    (self.windowy - self.bordersouth) - (bestfitend[1] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), fill = "#000000")
+                                    (self.windowy - self.bordersouth) - (bestfitend[1] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), fill="#000000")
             
         #line of best fit equation
         self.bestfitslope = "{0:4.4}".format(self.bestfitslope)
@@ -166,9 +170,9 @@ class linear_plot(object):
         self.bestfitequation = "Line of Best Fit Equation:\n" + str(self.originalyaxistitle) + " = " + str(self.bestfitslope) + " * " + str(self.xaxistitle) + " + " + str(self.bestfitintercept)
         if self.line_of_best_fit == True:
             if self.ypoints[len(self.xpoints)-1] < (self.yrangemax):
-                self.canvas.create_text((self.windowx - self.bordereast - self.graphx/5), (self.bordernorth + self.graphy/5), text = self.bestfitequation)
+                self.canvas.create_text((self.windowx - self.bordereast - self.graphx/5), (self.bordernorth + self.graphy/5), text=self.bestfitequation)
             else:
-                self.canvas.create_text((self.windowx - self.bordereast - self.graphx/5), (self.bordernorth + 4*self.graphy/5), text = self.bestfitequation)
+                self.canvas.create_text((self.windowx - self.bordereast - self.graphx/5), (self.bordernorth + 4*self.graphy/5), text=self.bestfitequation)
             
         #adding lines
         self.oldypoint = self.yrangemin
@@ -178,7 +182,7 @@ class linear_plot(object):
                 self.canvas.create_line((self.xpoints[point] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest,\
                                         (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin),\
                                         (self.oldxpoint - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest,\
-                                        (self.windowy - self.bordersouth) - (self.oldypoint - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), fill = self.linecolor)
+                                        (self.windowy - self.bordersouth) - (self.oldypoint - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), fill=self.linecolor)
                 self.oldypoint = self.ypoints[point]
                 self.oldxpoint = self.xpoints[point]
     
@@ -188,7 +192,7 @@ class linear_plot(object):
                 self.canvas.create_oval((self.xpoints[point] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest + 3,\
                                         (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin) + 3,\
                                         (self.xpoints[point] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest - 3,\
-                                        (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin) - 3, fill = "white")
+                                        (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin) - 3, fill="white")
                 
         #use an image?
         pointimages = []
@@ -196,7 +200,7 @@ class linear_plot(object):
             if self.image != None:
                 pointimages.append(tk.PhotoImage(file = self.image))
                 self.canvas.create_image((self.xpoints[point] - self.xrangemin)*(self.windowx - self.bordereast - self.borderwest)/(self.xrangemax - self.xrangemin) + self.borderwest,\
-                        (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), image = pointimages[point])
+                        (self.windowy - self.bordersouth) - (self.ypoints[point] - self.yrangemin)*(self.windowy - self.bordernorth - self.bordersouth)/(self.yrangemax - self.yrangemin), image=pointimages[point])
 
         #finalize
         self.canvas.create_line(self.bordernorth, self.borderwest, self.bordernorth, (self.windowy - self.bordersouth))
@@ -206,6 +210,15 @@ class linear_plot(object):
             return self.canvas
         else:
             tk.mainloop()
+
+    def residuals(self):
+        print(eval(self.bestfitslope), eval(self.bestfitslope)*10, eval(self.bestfitintercept))
+        for point in range(0,len(self.xpoints)):
+            self.residualsx.append(self.xpoints[point])
+            self.residualsy.append(self.ypoints[point] - (eval(self.bestfitslope)*self.xpoints[point]
+                                                          + eval(self.bestfitintercept)))
+        return self.residualsx, self.residualsy
+        
             
 if __name__ == "__main__":
     plot1 = linear_plot()
